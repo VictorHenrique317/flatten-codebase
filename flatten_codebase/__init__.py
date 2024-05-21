ae66567fc09925f91284414c712149678f113ac5
Victor <victor.henrique5800@gmail.com>
2024-05-21 17:04:28 -0300

    release

---
+++ flatten_codebase/__init__.py
@@ -0,0 +1,1 @@
+from flatten_codebase import *
--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='codebase-flatter',
-    version='1.1',
+    name='flatten-codebase',
+    version='1.2',
     packages=find_packages(),
-    description='Codebase Flatter simplifies codebase preparation for Language Models by converting it into a single Markdown file, making it easier for the developer to provide the codebase to the LM',
-    long_description=open('README.md').read(),
+    description="Flatten Codebase simplifies codebase preparation for Language Models by converting it into a single Markdown file, making it easier for the developer to provide the codebase to the LM",
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     author='Victor Henrique Silva Ribeiro',
     author_email='victor.henrique5800@gmail.com',
-    url='https://github.com/VictorHenrique317/codebase-flatter',
+    url='https://github.com/VictorHenrique317/flatten-codebase',
+    entry_points={
+        'console_scripts': [
+            'flatten-codebase=flatten_codebase.main:main',
+        ],
+    },
+    include_package_data=True,
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
     ],
-    entry_points={
-        'console_scripts': [
-            'flatten_codebase=flatten_codebase:main',
-        ],
-    },
 )
