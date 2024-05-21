c1c57a2210a62f52bcc037b7692bc5c672ee5c47
Victor <victor.henrique5800@gmail.com>
2024-05-21 11:50:18 -0300

    release

--- flatten_codebase.py
+++ flatten_codebase.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 def printFolderStructure(directory, output_file):
     output_file.write(f"### DIRECTORY {directory} FOLDER STRUCTURE ###\n")
@@ -16,22 +17,30 @@ def walkFolderTree(folder):
         for filename in filenames:
             yield os.path.join(dirpath, filename)
 
-base_folders = ['back/functions/src', 'front/src']
+def main():
+    parser = argparse.ArgumentParser(description='Flattens a codebase.')
+    parser.add_argument('--folders', nargs='+', required=True, help='Base folders to process')
+    
+    args = parser.parse_args()
+    base_folders = args.folders
+    
+    with open('codebase.md', 'w') as output_file:
+        for base_folder in base_folders:
+            printFolderStructure(base_folder, output_file)
+            
+            output_file.write(f"### DIRECTORY {base_folder} FLATTENED CONTENT ###\n")
+            for filepath in walkFolderTree(base_folder):
+                content = f"### {filepath} BEGIN ###\n"
+                
+                try:
+                    with open(filepath, "r") as f:
+                        content += f.read()
+                    content += f"\n### {filepath} END ###\n\n"
+                except:
+                    continue
+                
+                output_file.write(content)
+            output_file.write(f"### DIRECTORY {base_folder} FLATTENED CONTENT ###\n")
 
-with open('codebase.md', 'w') as output_file:
-    for base_folder in base_folders:
-        printFolderStructure(base_folder, output_file)
-        
-        output_file.write(f"### DIRECTORY {base_folder} FLATTENED CONTENT ###\n")
-        for filepath in walkFolderTree(base_folder):
-            content = f"### {filepath} BEGIN ###\n"
-
-            try:
-                with open(filepath, "r") as f:
-                    content += f.read()
-                content += f"\n### {filepath} END ###\n\n"
-            except:
-                continue
-
-            output_file.write(content)
-        output_file.write(f"### DIRECTORY {base_folder} FLATTENED CONTENT ###\n")
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='codebase-flatter',
+    version='0.1',
+    packages=find_packages(),
+    description='Codebase Flatter simplifies codebase preparation for Language Models by converting it into a single Markdown file, making it easier for the developer to provide the codebase to the LM',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Victor Henrique Silva Ribeiro',
+    author_email='victor.henrique5800@gmail.com',
+    url='https://github.com/VictorHenrique317/codebase-flatter',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
